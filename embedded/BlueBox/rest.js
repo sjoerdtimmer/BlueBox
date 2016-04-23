461a443b054516ec147932663033da34fdac2fbe
Sjoerd T. Timmer <themba@randomdata.nl>
2016-04-23 16:35:15 +0200

    new backend url

--- embedded/BlueBox/rest.js
+++ embedded/BlueBox/rest.js
@@ -1,14 +1,16 @@
 var rest = require('restler');
 //var serverip = "10.10.107.48";
-var serverip = "10.10.105.204";
+//var serverip = "10.10.105.204";
+var serverip = "40.118.109.71";
+var serverurl = "http://"+serverip+":8080/bluebox/servoy-service/rest_ws/bluebox/";
 module.exports = function(){
     this.postToSurvoy = function(field, value) {
         var data = {headers: { "Content-Type": "application/json" }};
         data[field] = value;
             
 
-        console.log("staring request");
-        rest.postJson("http://"+serverip+":8080/servoy-service/rest_ws/bluebox/"+field,data).on('success',function(data,resp){
+//        console.log("staring request");
+        rest.postJson(serverurl+field,data).on('success',function(data,resp){
             console.log(field+" data successfully POSTed: id="+data.id);
     //        console.log(data);
         }).on('fail',function(data,resp){
@@ -16,6 +18,6 @@ module.exports = function(){
         }).on('error',function(err,resp){
             console.error("POST failed!");
         })
-        console.log("req done");
+//        console.log("req done");
     };
 };
