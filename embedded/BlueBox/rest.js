77432eec7eb737ec0622517e24c2d036738e9502
Sjoerd T. Timmer <themba@randomdata.nl>
2016-04-23 15:10:49 +0200

    post to survoy seems to work

--- embedded/BlueBox/rest.js
+++ embedded/BlueBox/rest.js
@@ -1,14 +1,15 @@
 var rest = require('restler');
+var serverip = "10.10.107.48";
+//var serverip = "10.10.105.204";
 module.exports = function(){
     this.postToSurvoy = function(field, value) {
-        var data = {
-            data: { field: value },
-            headers: { "Content-Type": "application/json" }
-        };
+        var data = {headers: { "Content-Type": "application/json" }};
+        data[field] = value;
+            
 
         console.log("staring request");
-        rest.postJson("http://10.10.105.204:8080/servoy-service/rest_ws/bluebox/"+field,data).on('success',function(data,resp){
-            console.log(field+" data successfully POSTed: id="+resp.id);
+        rest.postJson("http://"+serverip+":8080/servoy-service/rest_ws/bluebox/"+field,data).on('success',function(data,resp){
+            console.log(field+" data successfully POSTed: id="+data.id);
     //        console.log(data);
         }).on('fail',function(data,resp){
             console.error("POST error!");
