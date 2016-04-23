62475cdd48c62549741d0fa0ac6f13041994a01b
Sjoerd T. Timmer <themba@randomdata.nl>
2016-04-23 13:16:14 +0200

    reading analog. next: post REST

---
+++ embedded/BlueBox/main.js
@@ -0,0 +1,12 @@
+/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jshint unused:true */
+// Leave the above lines for propper jshinting
+//Type Node.js Here :)
+
+var mraa = require('mraa'); //require mraa
+console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
+
+var analogPin0 = new mraa.Aio(0); //setup access analog input Analog pin #0 (A0)
+var analogValue = analogPin0.read(); //read the value of the analog pin
+console.log(analogValue); //write the value of the analog pin to the console
+
