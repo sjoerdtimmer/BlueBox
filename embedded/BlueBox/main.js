56eb07ea6fef3ae9585bf627854320d34ea3cf66
Sjoerd T. Timmer <themba@randomdata.nl>
2016-04-24 11:31:32 +0200

    removed debugging

--- embedded/BlueBox/main.js
+++ embedded/BlueBox/main.js
@@ -17,16 +17,16 @@ Article: https://software.intel.com/en-us/html5/articles/intel-xdk-iot-edition-n
 */
 
 
-var SegfaultHandler = require('segfault-handler');
-
-SegfaultHandler.registerHandler("crash.log"); // With no argument, SegfaultHandler will generate a generic log file name
-
-// Optionally specify a callback function for custom logging. This feature is currently only supported for Node.js >= v0.12 running on Linux.
-SegfaultHandler.registerHandler("crash.log", function(signal, address, stack) {
-    // Do what you want with the signal, address, or stack (array)
-    // This callback will execute before the signal is forwarded on.
-    console.log("segfault detected!");
-});
+//var SegfaultHandler = require('segfault-handler');
+//
+//SegfaultHandler.registerHandler("crash.log"); // With no argument, SegfaultHandler will generate a generic log file name
+//
+//// Optionally specify a callback function for custom logging. This feature is currently only supported for Node.js >= v0.12 running on Linux.
+//SegfaultHandler.registerHandler("crash.log", function(signal, address, stack) {
+//    // Do what you want with the signal, address, or stack (array)
+//    // This callback will execute before the signal is forwarded on.
+//    console.log("segfault detected!");
+//});
 
 
 
